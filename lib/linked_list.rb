377c5d1123b3c84438d99708f70dd91c2bb2cf56
Taylor <taylor@crucialartists.com>
2023-03-24 08:52:56 -0500

    Complete assignment

--- lib/linked_list.rb
+++ lib/linked_list.rb
@@ -2,4 +2,69 @@ require_relative './node'
 
 class LinkedList
 
+    attr_accessor :head
+
+    def initialize
+        @head = nil
+    end
+
+    def prepend(node)
+        node.next_node = head
+        self.head = node
+    end
+
+    def append(node)
+        return prepend(node) unless head
+
+        last_node = head
+        while last_node.next_node
+            last_node = last_node.next_node
+        end
+        last_node.next_node = node
+    end
+
+    def delete_head
+        return unless head
+        self.head = self.head.next_node
+    end
+
+    def delete_tail
+        return unless head
+        return delete_head if head.next_node.nil?
+
+        current = head
+        previous = nil
+        while current.next_node
+            previous = current
+            current = current.next_node
+        end
+        previous.next_node = nil
+    end
+
+    def add_after(index, node)
+        return prepend(node) if index == 0
+
+        current_index = 0
+        current = head
+        while current_index < index
+            current_index += 1
+            current = current.next_node
+        end
+        if current
+            node.next_node = current.next_node
+            current.next_node = node
+        else
+            append(node)
+        end
+    end
+
+    def search(value)
+        node = head
+        while node
+            return node if node.data == value
+            node = node.next_node
+        end
+        nil
+    end
+
 end
